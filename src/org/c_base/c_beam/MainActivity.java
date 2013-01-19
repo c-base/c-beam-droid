ca75fe5272cbc09deafcf09bbd6dd44a214b738d
sMiLe <smile@c3pb.de>
2013-01-19 03:27:17 +0100

    changed from creating an additional thread to using handler.postDelayed

--- src/org/c_base/c_beam/MainActivity.java
+++ src/org/c_base/c_beam/MainActivity.java
@@ -47,35 +47,39 @@ ActionBar.TabListener {
 	private static final int C_ONTROL_FRAGMENT = 4;
 	private static final int MISSION_FRAGMENT = 5;
 
-	private static final int threadDelay = 1000;
+	private static final int threadDelay = 5000;
+	private static final int firstThreadDelay = 1000;
 	private static final String TAG = "MainActivity";
 
-//	SectionsPagerAdapter mSectionsPagerAdapter;
+	ArrayList<Article> articleList;
+
+	//	SectionsPagerAdapter mSectionsPagerAdapter;
 	SectionsPagerAdapter mSectionsPagerAdapter;
 
 	ViewPager mViewPager;
 	private Handler handler;
 	EditText text;
-	
+
 	ActionBar actionBar;
 
 	C_beam c_beam = new C_beam(this);
 
-	Thread thread;
+	protected Runnable fred;
 	
+
 	public void setOnline() {
 		if (android.os.Build.VERSION.SDK_INT > 13) {
 			actionBar.setIcon(R.drawable.ic_launcher_c_beam_online);
 		}
 	}
-	
+
 	@SuppressLint("NewApi")
 	public void setOffline() {
 		if (android.os.Build.VERSION.SDK_INT > 13) {
 			actionBar.setIcon(R.drawable.ic_launcher);
 		}
 	}
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -88,16 +92,16 @@ ActionBar.TabListener {
 		}
 
 		setContentView(R.layout.activity_main);
-		
+
 		// Set up the action bar.
 		actionBar = getActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-//		BEGIN TEST Custom Font in Actionbar TEST BEGIN
-		
+		//		BEGIN TEST Custom Font in Actionbar TEST BEGIN
+
 		actionBar.setDisplayShowCustomEnabled(true);
 		actionBar.setDisplayShowTitleEnabled(false);
-		
+
 		LayoutInflater inflator = (LayoutInflater)this.getSystemService(getApplicationContext().LAYOUT_INFLATER_SERVICE);
 		View v = inflator.inflate(R.layout.view_actionbar, null);
 
@@ -107,18 +111,18 @@ ActionBar.TabListener {
 		((TextView)v.findViewById(R.id.title)).setPadding(10, 20, 10, 20);
 		actionBar.setCustomView(v);
 
-//		END TEST Custom Font in Actionbar TEST END
-		
-//		ViewPagerAdapter adapter = new ViewPagerAdapter( this );
-//	    ViewPager pager =
-//	        (ViewPager)findViewById( R.id.pager );
-//	    pager.setAdapter( adapter );
+		//		END TEST Custom Font in Actionbar TEST END
 
-//		mSectionsPagerAdapter = new MainPagerAdapter(
-//				getSupportFragmentManager());
-//
-//		
-//		
+		//		ViewPagerAdapter adapter = new ViewPagerAdapter( this );
+		//	    ViewPager pager =
+		//	        (ViewPager)findViewById( R.id.pager );
+		//	    pager.setAdapter( adapter );
+
+		//		mSectionsPagerAdapter = new MainPagerAdapter(
+		//				getSupportFragmentManager());
+		//
+		//		
+		//		
 		// Create the adapter that will return a fragment for each of the three
 		// primary sections of the app.
 		mSectionsPagerAdapter = new SectionsPagerAdapter(
@@ -143,7 +147,7 @@ ActionBar.TabListener {
 			TextView t = new TextView(getApplicationContext());
 			t.setTypeface(Typeface.createFromAsset(getAssets(), "CEVA-CM.TTF"));
 			tab.setText(mSectionsPagerAdapter.getPageTitle(i));
-//			tab.setCustomView(t);
+			//			tab.setCustomView(t);
 			tab.setTabListener(this);
 			actionBar.addTab(tab);
 
@@ -164,7 +168,7 @@ ActionBar.TabListener {
 						@Override
 						public void onClick(DialogInterface dialog, int whichButton)
 						{
-//							Toast.makeText(v.getContext(), "Du wirst eingelogt, bitte warten", Toast.LENGTH_LONG).show();
+							//							Toast.makeText(v.getContext(), "Du wirst eingelogt, bitte warten", Toast.LENGTH_LONG).show();
 							login();
 						}
 					});
@@ -200,7 +204,7 @@ ActionBar.TabListener {
 			}
 
 		});
-		
+
 		Button button_c_maps = (Button) findViewById(R.id.button_c_maps);
 		button_c_maps.setOnClickListener(new OnClickListener() {
 			@Override
@@ -213,10 +217,10 @@ ActionBar.TabListener {
 		// add extras here..
 		handler = new Handler();
 
-//		ArrayListFragment online = (ArrayListFragment) mSectionsPagerAdapter.getItem(0);
-//		online.setNextActivity(UserActivity.class);
-//		ArrayListFragment missions = (ArrayListFragment) mSectionsPagerAdapter.getItem(MISSION_FRAGMENT);
-//		missions.setNextActivity(MissionActivity.class);
+		//		ArrayListFragment online = (ArrayListFragment) mSectionsPagerAdapter.getItem(0);
+		//		online.setNextActivity(UserActivity.class);
+		//		ArrayListFragment missions = (ArrayListFragment) mSectionsPagerAdapter.getItem(MISSION_FRAGMENT);
+		//		missions.setNextActivity(MissionActivity.class);
 
 		if (sharedPref.getBoolean("pref_push", false)) {
 			GCMRegistrar.checkDevice(this);
@@ -225,27 +229,28 @@ ActionBar.TabListener {
 			if (regId.equals("")) {
 				GCMRegistrar.register(this, "987966345562");
 				regId = GCMRegistrar.getRegistrationId(this);
+				Log.i("GCM", "registering " + regId); 
 				c_beam.register(regId, sharedPref.getString("pref_username", "dummy"));
 			} else {
 				c_beam.register_update(regId, sharedPref.getString("pref_username", "dummy"));
-				Log.i("GCM", "Already registered");
+				Log.i("GCM", "Already registered"); 
 			}
 		}
-//		String font = sharedPref.getString("pref_font", "Android Default");
-//		if (font.equals("Default Android")) {
-//			
-//		} else if (font.equals("X-Scale")) {
-//			Typeface myTypeface = Typeface.createFromAsset(getAssets(), "X-SCALE.TTF");
-//			final ViewGroup mContainer = (ViewGroup) findViewById(
-//					android.R.id.content).getRootView();
-//			setAppFont(mContainer, myTypeface);
-//		} else if (font.equals("Ceva")) {	
-//			Typeface myTypeface = Typeface.createFromAsset(getAssets(), "CEVA-CM.TTF");
-//			final ViewGroup mContainer = (ViewGroup) findViewById(
-//					android.R.id.content).getRootView();
-//			setAppFont(mContainer, myTypeface);
-//			
-//		}
+		//		String font = sharedPref.getString("pref_font", "Android Default");
+		//		if (font.equals("Default Android")) {
+		//			
+		//		} else if (font.equals("X-Scale")) {
+		//			Typeface myTypeface = Typeface.createFromAsset(getAssets(), "X-SCALE.TTF");
+		//			final ViewGroup mContainer = (ViewGroup) findViewById(
+		//					android.R.id.content).getRootView();
+		//			setAppFont(mContainer, myTypeface);
+		//		} else if (font.equals("Ceva")) {	
+		//			Typeface myTypeface = Typeface.createFromAsset(getAssets(), "CEVA-CM.TTF");
+		//			final ViewGroup mContainer = (ViewGroup) findViewById(
+		//					android.R.id.content).getRootView();
+		//			setAppFont(mContainer, myTypeface);
+		//			
+		//		}
 	}
 
 	public void onStart() {
@@ -255,12 +260,12 @@ ActionBar.TabListener {
 		updateLists();
 	}
 
-	
+
 	@Override
 	protected void onPause() {
 		Log.i(TAG, "onPause()");
 		super.onPause();
-		thread.interrupt();
+//		thread.interrupt();
 		c_beam.stopThread();
 	}
 
@@ -268,7 +273,7 @@ ActionBar.TabListener {
 	protected void onStop() {
 		Log.i(TAG, "onStop()");
 		super.onStop();
-		thread.interrupt();
+//		thread.interrupt();
 		c_beam.stopThread();
 	}
 
@@ -280,29 +285,29 @@ ActionBar.TabListener {
 		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
 		c_beam.force_logout(sharedPref.getString("pref_username", "bernd"));
 	}
-//	public void c_out() {
-//		AlertDialog.Builder b = new AlertDialog.Builder(this);
-//		b.setTitle("c_out-durchsage eingeben");
-//		final EditText input = new EditText(this);
-//		b.setView(input);
-//		b.setPositiveButton("OK", new DialogInterface.OnClickListener()
-//		{
-//			@Override
-//			public void onClick(DialogInterface dialog, int whichButton)
-//			{
-//				String result = input.getText().toString();
-//				c_beam.tts(result);
-//				Log.i("c_out", result);
-//			}
-//		});
-//		b.setNegativeButton("CANCEL", null);
-//		b.create().show();
-//	}
-	
+	//	public void c_out() {
+	//		AlertDialog.Builder b = new AlertDialog.Builder(this);
+	//		b.setTitle("c_out-durchsage eingeben");
+	//		final EditText input = new EditText(this);
+	//		b.setView(input);
+	//		b.setPositiveButton("OK", new DialogInterface.OnClickListener()
+	//		{
+	//			@Override
+	//			public void onClick(DialogInterface dialog, int whichButton)
+	//			{
+	//				String result = input.getText().toString();
+	//				c_beam.tts(result);
+	//				Log.i("c_out", result);
+	//			}
+	//		});
+	//		b.setNegativeButton("CANCEL", null);
+	//		b.create().show();
+	//	}
+
 	public void updateLists() {
 		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-//		if (true)
-//			return;
+		//		if (true)
+		//			return;
 		UserListFragment online = (UserListFragment) mSectionsPagerAdapter.getItem(USER_FRAGMENT);
 		EventListFragment events = (EventListFragment) mSectionsPagerAdapter.getItem(EVENTS_FRAGMENT);
 		MissionListFragment missions = (MissionListFragment) mSectionsPagerAdapter.getItem(MISSION_FRAGMENT);
@@ -354,18 +359,19 @@ ActionBar.TabListener {
 			}
 
 			if(c_portal.isAdded()) {
-				ArrayList<Article> articleList = new ArrayList<Article>();
-				articleList = c_beam.getArticles();
-				c_portal.clear();
-				for(int i=0; i<articleList.size();i++)
-					c_portal.addItem(articleList.get(i));
+				ArrayList<Article> tmpList = c_beam.getArticles();
+				if (articleList == null || 1 == 1 || articleList.size() != tmpList.size()) {
+					articleList = tmpList;
+					c_portal.clear();
+					for(int i=0; i<articleList.size();i++)
+						c_portal.addItem(articleList.get(i));
+				}
 			}
 
 			if(artefacts.isAdded()) {
 				ArrayList<Artefact> artefactList = new ArrayList<Artefact>();
 				artefactList = c_beam.getArtefacts();
 				if (artefactList.size() != artefacts.size()) {
-					Log.i("artefacts", "UPDATE: "+ artefactList.size() + " / "+ artefacts.size());
 					//artefacts.setArrayList(artefactList);
 					artefacts.clear();
 					for(int i=0; i<artefactList.size();i++)
@@ -381,36 +387,24 @@ ActionBar.TabListener {
 
 	public void startProgress() {
 		// Do something long
-		Runnable runnable = new Runnable() {
+		fred = new Runnable() {
 			@Override
 			public void run() {
-				boolean stop = false;
-				while(!stop) {
-					try {
-						Thread.sleep(threadDelay);
-					} catch (InterruptedException e) {
-						stop = true;
-					}
-					handler.post(new Runnable() {
-						@Override
-						public void run() {
-							updateLists();
-							if (c_beam.isInCrewNetwork())
-								setOnline();
-							else
-								setOffline();
-						}
-					});
-				}
+				updateLists();
+				if (c_beam.isInCrewNetwork())
+					setOnline();
+				else
+					setOffline();
+				handler.postDelayed(fred, threadDelay);
 			}
+
 		};
-		thread = new Thread(runnable);
-//		thread.start();
+		handler.postDelayed(fred, firstThreadDelay );
 	}
 	protected void onResume () {
 		Log.i(TAG, "onResume()");
 		super.onResume();
-		thread.start();
+//		thread.start();
 		c_beam.startThread();
 		updateLists();
 	}
@@ -532,30 +526,30 @@ ActionBar.TabListener {
 			return null;
 		}
 	}	
-	
+
 	public static final void setAppFont(ViewGroup mContainer, Typeface mFont)
 	{
-	    if (mContainer == null || mFont == null) return;
+		if (mContainer == null || mFont == null) return;
 
-	    final int mCount = mContainer.getChildCount();
+		final int mCount = mContainer.getChildCount();
 
-	    // Loop through all of the children.
-	    for (int i = 0; i < mCount; ++i)
-	    {
-	        final View mChild = mContainer.getChildAt(i);
-	        if (mChild instanceof TextView)
-	        {
-	            // Set the font if it is a TextView.
-	            ((TextView) mChild).setTypeface(mFont);
-	        }
-	        else if (mChild instanceof ViewGroup)
-	        {
-	            // Recursively attempt another ViewGroup.
-	            setAppFont((ViewGroup) mChild, mFont);
-	        }
-	    }
-	    Log.i("MainActivity", "font set");
-	    
+		// Loop through all of the children.
+		for (int i = 0; i < mCount; ++i)
+		{
+			final View mChild = mContainer.getChildAt(i);
+			if (mChild instanceof TextView)
+			{
+				// Set the font if it is a TextView.
+				((TextView) mChild).setTypeface(mFont);
+			}
+			else if (mChild instanceof ViewGroup)
+			{
+				// Recursively attempt another ViewGroup.
+				setAppFont((ViewGroup) mChild, mFont);
+			}
+		}
+		Log.i("MainActivity", "font set");
+
 	}
 
 }
