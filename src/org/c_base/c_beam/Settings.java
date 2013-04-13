1e163b785cbd6c1f12d6e6ba23f8d581cb505a8b
cmile <github@cmile.de>
2013-04-13 12:11:01 +0200

    removed password, added stats_enabled

--- src/org/c_base/c_beam/Settings.java
+++ src/org/c_base/c_beam/Settings.java
@@ -3,10 +3,11 @@ package org.c_base.c_beam;
 public interface Settings {
 	public static final String PUSH = "pref_push";
 	public static final String USERNAME = "pref_username";
-	public static final String PASSWORD = "pref_password";
+//	public static final String PASSWORD = "pref_password";
 	public static final String FONT = "pref_font";
 	public static final String FONT_SIZE = "pref_fontsize";
 	public static final String FADE_USERS = "pref_fade_users";
 	public static final String C_THEME = "pref_c_theme";
 	public static final String DISPLAY_AP = "pref_display_ap";
+	public static final String STATS_ENABLED = "pref_stats_enabled";
 }
