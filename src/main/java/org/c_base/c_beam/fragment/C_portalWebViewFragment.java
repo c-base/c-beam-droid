0f737c1de30f2e71659279018e38e7e4b4f31b0b
cmile <github@cmile.de>
2018-02-14 10:02:04 +0100

    fixed WebView SSL Error Handler alerts

--- src/main/java/org/c_base/c_beam/fragment/C_portalWebViewFragment.java
+++ src/main/java/org/c_base/c_beam/fragment/C_portalWebViewFragment.java
@@ -1,5 +1,6 @@
 package org.c_base.c_beam.fragment;
 
+import android.net.http.SslCertificate;
 import android.net.http.SslError;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,6 +13,17 @@ import android.webkit.WebViewClient;
 
 import org.c_base.c_beam.R;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SignatureException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+
 public class C_portalWebViewFragment extends Fragment {
     protected WebView webView;
     private String url;
@@ -22,15 +34,41 @@ public class C_portalWebViewFragment extends Fragment {
             return null;
         }
 
-        View v = inflater.inflate(R.layout.fragment_c_portal_webview, container, false);
-        webView = (WebView) v.findViewById(R.id.c_portalWebView);
-        webView.setWebViewClient(new WebViewClient() {
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                handler.proceed();
-            }
-        });
-        webView.getSettings().setJavaScriptEnabled(true);
-        return v;
+        CertificateFactory cf = null;
+        try {
+            cf = CertificateFactory.getInstance("X.509");
+            InputStream caInput = getResources().openRawResource(R.raw.cacert_class3); // stored at \app\src\main\res\raw
+            final Certificate certificate = cf.generateCertificate(caInput);
+            caInput.close();
+
+            View v = inflater.inflate(R.layout.fragment_c_portal_webview, container, false);
+            webView = (WebView) v.findViewById(R.id.c_portalWebView);
+            webView.setWebViewClient(new WebViewClient() {
+                public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                    SslCertificate sslCertificate = error.getCertificate();
+                    Certificate cert = getX509Certificate(sslCertificate);
+                    if (cert != null && certificate != null) {
+                        try {
+                            // Reference: https://developer.android.com/reference/java/security/cert/Certificate.html#verify(java.security.PublicKey)
+                            cert.verify(certificate.getPublicKey()); // Verify here...
+                            handler.proceed();
+                        } catch (CertificateException | NoSuchAlgorithmException | InvalidKeyException | NoSuchProviderException | SignatureException e) {
+                            super.onReceivedSslError(view, handler, error);
+                            e.printStackTrace();
+                        }
+                    } else {
+                        super.onReceivedSslError(view, handler, error);
+                    }
+                }
+            });
+            webView.getSettings().setJavaScriptEnabled(true);
+            return v;
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
@@ -45,6 +83,21 @@ public class C_portalWebViewFragment extends Fragment {
         this.url = url;
         if (webView != null) {
             webView.loadUrl(url);
+        }
+    }
+
+    private Certificate getX509Certificate(SslCertificate sslCertificate) {
+        Bundle bundle = SslCertificate.saveState(sslCertificate);
+        byte[] bytes = bundle.getByteArray("x509-certificate");
+        if (bytes == null) {
+            return null;
+        } else {
+            try {
+                CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+                return certFactory.generateCertificate(new ByteArrayInputStream(bytes));
+            } catch (CertificateException e) {
+                return null;
+            }
         }
     }
 
