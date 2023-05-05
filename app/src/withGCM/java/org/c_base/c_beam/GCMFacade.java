9115576be033fe5b33a32a8fc3e71efb3d3837a2
saces <saces@c-base.org>
2023-05-05 21:31:03 +0200

    update goggle/gcm

--- app/src/withGCM/java/org/c_base/c_beam/GCMFacade.java
+++ app/src/withGCM/java/org/c_base/c_beam/GCMFacade.java
@@ -7,28 +7,28 @@ import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import org.c_base.c_beam.task.AsyncC_beamTask;
 
 public class GCMFacade  {
     public static void setupGCM(final Context context) {
-        FirebaseInstanceId.getInstance().getInstanceId()
-                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
                     @Override
-                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                    public void onComplete(@NonNull Task<String> task) {
                         if (!task.isSuccessful()) {
                             return;
                         }
                         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
                         String username = sharedPref.getString(Settings.USERNAME, "bernd");
                         // Get new Instance ID token
-                        String token = task.getResult().getToken();
+                        String token = task.getResult();
                         AsyncC_beamTask act = new AsyncC_beamTask();
                         act.execute("fcm_update", "user", username, "regid", token);
                         // C_beam.getInstance().call("fcm_update", "user", username, "regid", token);;
                     }
                 });
     }
+
 }
