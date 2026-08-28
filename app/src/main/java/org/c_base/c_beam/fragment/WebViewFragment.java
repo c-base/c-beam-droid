4645cb7135cead7ad86f2ad915253c034ab9dd89
cmile <cmile@cmile.de>
2026-08-29 00:00:16 +0200

    fix unsafe WebView SSL error handler (Google policy alert)

--- app/src/main/java/org/c_base/c_beam/fragment/WebViewFragment.java
+++ app/src/main/java/org/c_base/c_beam/fragment/WebViewFragment.java
@@ -4,18 +4,13 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import org.c_base.c_beam.R;
-
-import java.io.InputStream;
-import java.security.cert.CertificateFactory;
 
 public class WebViewFragment extends Fragment {
 	private WebView webView;
@@ -34,23 +29,11 @@ public class WebViewFragment extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_webview, container, false);
 		webView = v.findViewById(R.id.webView);
 
-		try (InputStream caInput = getResources().openRawResource(R.raw.cacert)) {
-            CertificateFactory cf = CertificateFactory.getInstance("X.509");
-            cf.generateCertificate(caInput);
+		// Keep navigation inside the WebView, but leave SSL error handling to the
+		// default implementation, which cancels the load on an invalid certificate.
+		webView.setWebViewClient(new WebViewClient());
+		initWebView();
 
-            webView.setWebViewClient(new WebViewClient() {
-                @Override
-                public void onReceivedSslError(WebView view, SslErrorHandler handler, android.net.http.SslError error) {
-                    handler.proceed();
-                }
-            });
-
-            initWebView();
-        } catch (Exception e) {
-            Log.e("WebViewFragment", "Error initializing WebView with custom CA", e);
-            webView.setWebViewClient(new WebViewClient());
-            initWebView();
-        }
 		return v;
 	}
 
